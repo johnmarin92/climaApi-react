17f03a568db3c28be8fd459181e384503df3f841
johnmarin92 <bxeneixe@gmail.co>
2021-03-09 18:33:56 -0300

    http-https

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ function App() {
 
         if(consultar) {
           const appId = '508e511638b3e6e82a9c0083ad775dd4';
-          const url = `http://api.openweathermap.org/data/2.5/weather?q=${ciudad},${pais}&appid=${appId}`;
+          const url = `https://api.openweathermap.org/data/2.5/weather?q=${ciudad},${pais}&appid=${appId}`;
   
           const respuesta = await fetch(url);
           const resultado = await respuesta.json();
